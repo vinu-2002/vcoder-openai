c32b1e28ff8d60f8557ce878a139904ea6c92775
vinayak <vinayak>
2023-07-02 16:19:47 +0530

    irs

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://vcoder-604l.onrender.com/', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
